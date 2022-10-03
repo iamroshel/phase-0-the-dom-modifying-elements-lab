c6703aa9494928bdc2c58dba18c973a330f665bf
Roshel Esteron <mynameisroshel19@gmail.com>
2022-10-03 16:24:25 +1000

    Completed Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.querySelector("main");
+element.remove();
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory"
+
+newHeader.textContent = " Roshel is the champion"
+
+
+
+
